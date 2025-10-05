6cc57f26bace323b87de891243a530a237f195e4
unknown <z5532071@ad.unsw.edu.au>
2025-10-05 19:01:04 +0800

    Finish dark modle background

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Theme from '../components/Navigation/Navbar/Theme'
+import DarkBackground from "../components/DarkBackground"
 
 const Home = () => {
   return (
     <div>
       <Theme/>
+      <DarkBackground/>
       <h1 className="text-4xl font-bold">Welcome to the Home Page</h1>
       <p className="mt-4 text-lg">This is the home page of our application.</p>
     </div>
