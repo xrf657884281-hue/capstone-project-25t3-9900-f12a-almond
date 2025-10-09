bc5c05e2f32baba832ebbc92f06b315e4844ca5b
JinLiuluofeng <jin_addaamm@163.com>
2025-10-09 12:09:28 +1100

    fix wrong routes

--- src/constants/index.tsx
+++ src/constants/index.tsx
@@ -2,18 +2,14 @@ import About from "../pages/About";
 import Detection from "../pages/Detection";
 import Generate from "../pages/Generate";
 import Home from "../pages/Home";
-import SignIn from "../pages/Sign-in"; 
-import SignUp from "../pages/Sign-up"; 
+
 
 export const NavRoutes = [
   { path: "/", element: <Home /> },
-  { path: "/sign-in", element: <SignIn /> },
-  { path: "/sign-up", element: <SignUp /> },
   { path: "/Detection", element: <Detection /> },
   { path: "/Generate", element: <Generate /> },
   { path: "/About", element: <About /> },
 ];
-
 
 export const navItems = [
   { path: "/Detection", label: "Detection" },
